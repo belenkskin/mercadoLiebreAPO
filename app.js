8a982eefd83f9f8525103e40b2f0e24ad4d9054a
belenkskin <belenkskin@hotmail.com>
2022-04-25 12:46:49 -0300

    comento variable psot

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const PORT = 3000;
+/* const PORT = 3000; */
 /*
 se inicia con npm star en la terminal
 */
 
-app.listen(process.env.PORT || 3000, function(){ console.log('Servidor corriendo en el puerto 3000')});
+app.listen(process.env.PORT || 3000, function () { console.log('Servidor corriendo en el puerto 3000') });
 
 /* app.listen(PORT, () => { console.log('Servidor corriendo en el puerto:' + PORT) }) */
 
